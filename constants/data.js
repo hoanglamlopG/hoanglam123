72aa8fed2c2410f5db6a37b388d4936a0096cb34
unknown <bipun2806@gmail.com>
2023-12-26 20:24:52 +0700

    first commit

---
+++ constants/data.js
@@ -0,0 +1,75 @@
+import images from "./images";
+
+export const latestList = [
+    {
+      id: "1",
+      image: images.shirt1,
+      name: "Nike ",
+      category: "Áo Hoodie",
+      oldPrice: 110,
+      price: 110
+    },
+    {
+      id: "2",
+      image: images.shirt2,
+      name: "Nike",
+      category: "Áo thể theo",
+      oldPrice: 50,
+      price: 30
+    },
+    {
+      id: "3",
+      image: images.shirt2,
+      name: "Nike",
+      category: "Áo thể thao",
+      oldPrice: 130,
+      price: 130
+    }
+  ]
+export const shoesList1 = [
+    {
+      id: "1",
+      shoes: images.shoes1,
+    },
+    {
+      id: "2",
+      shoes: images.shoes2,
+    },
+    {
+      id: "3",
+      shoes: images.shoes3,
+    },
+    {
+      id: "4",
+      shoes: images.shoes4,
+    },
+    {
+      id: "5",
+      shoes: images.shoes5,
+    }
+  ]
+  
+export const shoesList2 = [
+    {
+      id: "1",
+      shoes: images.shoes3,
+    },
+    {
+      id: "2",
+      shoes: images.shoes1,
+    },
+    {
+      id: "3",
+      shoes: images.shoes2,
+    },
+    {
+      id: "4",
+      shoes: images.shoes4,
+    },
+    {
+      id: "5",
+      shoes: images.shoes5,
+    }
+    
+    
+  ]
